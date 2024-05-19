5824afab0c3d17b0e53ff9e08f52949e73bc6199
Yashjain1623 <157748380+Yashjain1623@users.noreply.github.com>
2024-05-19 17:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const byteSize = (str) => {
-  // write your code here
+  // write your coconst byteSize = (str) => {
+  // Create a Blob object with the input string
+  const blob = new Blob([str]);
+  // Return the size of the Blob, which represents the byte size of the string
+  return blob.size;
 };
 
+// 
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
